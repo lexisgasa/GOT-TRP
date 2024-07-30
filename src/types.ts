b4f84575638447129d8663084d8829603872efd2
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-07-30 18:35:24 +0200

    Create Fighter interface

--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,9 @@ interface King extends Character {
   yearsOfReign: number;
   talk: () => "Vais a morir todos";
 }
+
+interface Fighter extends Character {
+  weapon: string;
+  dexterity: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+  talk: () => "Primero pego y luego pregunto";
+}
